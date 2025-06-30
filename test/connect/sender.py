b94515af4d422704bbbb5cd68b99c6fe18da71e2
blueplanet <blueplanet081@outlook.com>
2025-06-30 22:07:39 +0900

    wlan_infoに修正

--- test/connect/sender.py
+++ test/connect/sender.py
@@ -3,17 +3,19 @@ import network
 import socket
 import time
 import random # 温度シミュレーション用
-from wifi_config import SSID, PASSWORD
+# from wifi_config import SSID, PASSWORD
+from wlan_info import ssid, passwd
+
 
 # 受信側のPico WのIPアドレスに置き換えてください！
 # 受信側を起動し、そのPico WのIPアドレスを確認してからここに設定してください。
-RECEIVER_IP = "受信側のPico WのIPアドレス"
+RECEIVER_IP = "192.168.0.38"
 RECEIVER_PORT = 12345 # 受信側と合わせる
 
 # Wi-Fi接続
 wlan = network.WLAN(network.STA_IF)
 wlan.active(True)
-wlan.connect(SSID, PASSWORD)
+wlan.connect(ssid, passwd)
 
 # 接続待機
 max_wait = 10
